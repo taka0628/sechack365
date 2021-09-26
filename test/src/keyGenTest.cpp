1fb9a53f96da59c20a212818d488fba7defb62c0
taka <is0516rs@ed.ritsumei.ac.jp>
2021-09-26 14:18:02 +0900

    [add]追加

---
+++ test/src/keyGenTest.cpp
@@ -0,0 +1,99 @@
+#include "../include/test.hpp"
+
+using namespace std;
+
+TEST(keyGenMethodTest, setUsbId)
+{
+	key_gen_c key;
+	ASSERT_TRUE(key.set_usbID("1d6b:0002"));
+	ASSERT_FALSE(key.set_usbID(""));
+	ASSERT_FALSE(key.set_usbID("hoge"));
+	ASSERT_FALSE(key.set_usbID("1d6b"));
+	ASSERT_FALSE(key.set_usbID("0002"));
+	ASSERT_FALSE(key.set_usbID("1d6:0002"));
+}
+
+TEST(keyGenMethodTest, setPass)
+{
+	key_gen_c key;
+	ASSERT_TRUE(key.set_pass("hoge"));
+	ASSERT_FALSE(key.set_pass(""));
+}
+
+TEST(keyGenMethodTest, canKeyGen)
+{
+	key_gen_c key;
+	ASSERT_FALSE(key.canKeyGen());
+	ASSERT_TRUE(key.set_pass("hoge"));
+	ASSERT_FALSE(key.canKeyGen());
+	ASSERT_TRUE(key.set_usbID("1d6b:0002"));
+	ASSERT_TRUE(key.set_UsbSerial());
+	ASSERT_TRUE(key.canKeyGen());
+}
+
+TEST(errorLogTest, printError)
+{
+	key_gen_c key;
+	PRINT_ERROR_LOG("test");
+}
+
+TEST(keyGenMethodTest, setUsbSerial)
+{
+	key_gen_c key;
+	ASSERT_FALSE(key.set_UsbSerial());
+	ASSERT_TRUE(key.set_usbID("1d6b:0002"));
+	ASSERT_TRUE(key.set_UsbSerial());
+}
+
+TEST(keyGenMethodTest, nonce)
+{
+	key_gen_c key;
+	dynamic_mem_c buf;
+	ASSERT_TRUE(key.set_nonce());
+	ASSERT_FALSE(key.get_nonce(buf));
+
+	buf.d_new(1);
+	ASSERT_FALSE(key.get_nonce(buf));
+	buf.d_new(NONCE_SIZE - 1);
+	ASSERT_FALSE(key.get_nonce(buf));
+
+	buf.d_new(NONCE_SIZE);
+	ASSERT_TRUE(key.get_nonce(buf));
+	buf.d_new(NONCE_SIZE * 2);
+	ASSERT_FALSE(key.get_nonce(buf));
+}
+
+TEST(keyGenMethodTest, keyGen)
+{
+	key_gen_c key;
+	EXPECT_FALSE(key.new_key_gen());
+
+	ASSERT_TRUE(key.set_pass("hoge"));
+	EXPECT_FALSE(key.new_key_gen());
+
+	ASSERT_TRUE(key.set_usbID("1d6b:0002"));
+	EXPECT_FALSE(key.new_key_gen());
+
+	ASSERT_TRUE(key.set_UsbSerial());
+	ASSERT_TRUE(key.new_key_gen());
+}
+
+TEST(keyGenMethodTest, getKey)
+{
+	key_gen_c key;
+	ASSERT_TRUE(key.set_pass("hoge"));
+	ASSERT_TRUE(key.set_usbID("1d6b:0002"));
+	ASSERT_TRUE(key.set_UsbSerial());
+	ASSERT_TRUE(key.new_key_gen());
+
+	dynamic_mem_c temp;
+	temp.d_new(AES_SIZE);
+	ASSERT_TRUE(key.get_key(temp));
+	cout << "key";
+	SHA_c sha;
+	for (size_t i = 0; i < temp.get_size(); i++)
+	{
+		cout << sha.str2hex(temp.mem[i]);
+	}
+	cout << endl;
+}
