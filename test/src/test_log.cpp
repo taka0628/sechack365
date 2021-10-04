4519eb5c88f5fa8cf72b0b637522022416a61cff
taka <is0516rs@ed.ritsumei.ac.jp>
2021-10-04 15:47:04 +0900

    [fix]テスト用ログの書き方を修正

--- test/src/test_log.cpp
+++ test/src/test_log.cpp
@@ -2,7 +2,11 @@
 
 using namespace std;
 
-TestLog_c::TestLog_c(string comment)
+TestLog_c::TestLog_c()
+{
+}
+
+TestLog_c::TestLog_c(const string comment)
 {
 	string text = "===";
 	text += comment;
@@ -13,4 +17,20 @@ TestLog_c::TestLog_c(string comment)
 TestLog_c::~TestLog_c()
 {
 	PRINT_ERROR_LOG("===TEST END===");
+}
+
+void TestLog_c::correctTest()
+{
+	string text = "===";
+	text += "Correct Test";
+	text += "===";
+	PRINT_ERROR_LOG(text.c_str());
+}
+
+void TestLog_c::errorTest()
+{
+	string text = "===";
+	text += "Error Test";
+	text += "===";
+	PRINT_ERROR_LOG(text.c_str());
 }
