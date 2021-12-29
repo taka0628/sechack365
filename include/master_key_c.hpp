d2336c27a0fa85ecf5fc0e2786222fe3384c9520
taka0628 <takatsun15@gmail.com>
2021-12-29 18:47:58 +0900

    [add]masterキーを扱うクラスを作成

---
+++ include/master_key_c.hpp
@@ -0,0 +1,34 @@
+#ifndef ___MASTER_KEY_HPP
+#define ___MASTER_KEY_HPP
+
+#include "aes_c.hpp"
+#include "macro.hpp"
+#include "openssl/rand.h"
+#include "sha_c.hpp"
+
+#include <cstdio>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+
+class master_key_c {
+private:
+    master_key_c(const master_key_c&);
+    std::vector<u_char> master_key_;
+
+public:
+    master_key_c(/* args */);
+    ~master_key_c();
+    // Mkeyが正しく取得できない場合は異常終了
+    std::vector<u_char> get_master_key() const;
+    // 認証情報を含みMKeyをリセット
+    bool reset();
+    // Mkeyが復号され、メンバ変数に正しく格納されているか確認
+    bool isCorrect() const;
+};
+
+#endif
--- src/master_key_c.cpp
+++ src/master_key_c.cpp
@@ -1,4 +1,5 @@
-#include "../include/master_key_c.cpp"
+#include "../include/master_key_c.hpp"
+#include "../include/error.hpp"
 
 master_key_c::master_key_c(/* args */)
 {
@@ -6,4 +7,27 @@ master_key_c::master_key_c(/* args */)
 
 master_key_c::~master_key_c()
 {
+}
+
+using namespace std;
+
+bool master_key_c::isCorrect() const
+{
+    if (this->master_key_.empty() == true) {
+        return false;
+    }
+    if (this->master_key_.size() != AES_SIZE) {
+        return false;
+    }
+    return true;
+}
+
+vector<u_char> master_key_c::get_master_key() const
+{
+    if (this->isCorrect() == false) {
+        ERROR("master key is false");
+        log::push_value("master_key", this->master_key_);
+        exit(1);
+    }
+    return this->master_key_;
 }
