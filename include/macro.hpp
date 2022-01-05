4fcaf1ab5f595c85c0402f96cebdac8a29cbaafc
taka0628 <takatsun15@gmail.com>
2022-01-05 15:30:00 +0900

    [add]変数名を出力するマクロを作成

--- include/macro.hpp
+++ include/macro.hpp
@@ -8,6 +8,9 @@
     printf("[ERROR]\n\t__%s__/__%s: %d\n\t%s\n", __FILE__, __func__, __LINE__, comment); \
     log::push_log(comment, __FILE__, __func__, __LINE__)
 
+#define PUSH_VALUE(arg) \
+    log::push_value(TO_STRING(arg), arg);
+
 #define ERROR_NO_COMMENT                                                  \
     printf("[ERROR]\n\t__%s__/__%s: %d\n", __FILE__, __func__, __LINE__); \
     log::push_log("", __FILE__, __func__, __LINE__)
