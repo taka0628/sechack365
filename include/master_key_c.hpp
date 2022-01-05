3cbf91b48d779c7995d103ff3c6580d578ffc0c7
taka0628 <takatsun15@gmail.com>
2022-01-05 21:42:54 +0900

    [fix]初期化を行うメソッドを作成

--- include/master_key_c.hpp
+++ include/master_key_c.hpp
@@ -3,6 +3,7 @@
 
 #include "aes_c.hpp"
 #include "device_c.hpp"
+#include "error.hpp"
 #include "key_gen.hpp"
 #include "macro.hpp"
 #include "openssl/rand.h"
@@ -21,13 +22,19 @@
 class master_key_c {
 private:
     master_key_c(const master_key_c&);
-    std::vector<u_char> master_key_;
+    dynamic_mem_c master_key_;
+    // 暗号化された鍵をファイルへ出力
+    bool push_key(const dynamic_mem_c enc_mkey, const dynamic_mem_c iv, const dynamic_mem_c key) const;
+    // ファイルから暗号化された鍵を取得
+    dynamic_mem_c get_key_from_keylist(const dynamic_mem_c& key_hash) const;
+    // mkeyを暗号化し、ファイルへ保存
+    bool enc_mkey(const dynamic_mem_c key) const;
 
 public:
     master_key_c();
     ~master_key_c();
     // Mkeyが正しく取得できない場合は異常終了
-    std::vector<u_char> get_key() const;
+    dynamic_mem_c get_key() const;
     // Mkeyのハッシュ値を取得
     std::vector<u_char> get_hash() const;
     // 認証情報を含みMKeyをリセット
--- src/master_key_c.cpp
+++ src/master_key_c.cpp
@@ -1,9 +1,8 @@
 #include "../include/master_key_c.hpp"
-#include "../include/error.hpp"
 
 master_key_c::master_key_c()
 {
-    this->master_key_.clear();
+    this->master_key_.d_free();
 }
 
 master_key_c::~master_key_c()
@@ -23,14 +22,47 @@ bool master_key_c::isCorrect() const
     return true;
 }
 
-vector<u_char> master_key_c::get_key() const
+dynamic_mem_c master_key_c::get_key() const
 {
     if (this->isCorrect() == false) {
         ERROR("master key is false");
-        log::push_value(TO_STRING(master_key_), this->master_key_);
+        log::push_value("this->master_key_.size()", this->master_key_.size());
         exit(1);
     }
     return this->master_key_;
+}
+
+bool master_key_c::push_key(const dynamic_mem_c enc_mkey, const dynamic_mem_c iv, const dynamic_mem_c key) const
+{
+    if (enc_mkey.size() != AES_SIZE || iv.size() != AES_SIZE || key.size() != AES_SIZE) {
+        ERROR("鍵のサイズが不正");
+        log::push_value("enc_mkey.size()", enc_mkey.size());
+        log::push_value("iv.size()", iv.size());
+        log::push_value("key.size()", key.size());
+        return false;
+    }
+    // 鍵のハッシュ値
+    dynamic_mem_c hash_key(HASH_SIZE);
+    SHA_c sha;
+    if (!sha.sha2_cal(enc_mkey, hash_key, SHA_c::SHA2_bit::SHA_256)) {
+        ERROR("ハッシュ計算エラー");
+        return false;
+    }
+    if (hash_key.size() != HASH_SIZE) {
+        ERROR("ハッシュサイズエラー");
+        log::push_value("hash_key.size()", hash_key.size());
+        return false;
+    }
+
+    file_ptr_c fp;
+    if (!fp.open(MASTER_KEY_LIST_FILE, "wb")) {
+        ERROR("ファイルが開けません");
+        return false;
+    }
+    fwrite(hash_key.mem_, 1, hash_key.size(), fp.fp_);
+    fwrite(enc_mkey.mem_, 1, enc_mkey.size(), fp.fp_);
+    fwrite(iv.mem_, 1, iv.size(), fp.fp_);
+    return true;
 }
 
 bool master_key_c::init(string usb_id, string pass_word)
@@ -45,6 +77,7 @@ bool master_key_c::init(string usb_id, string pass_word)
     dynamic_mem_c mkey(AES_SIZE);
     RAND_bytes(mkey.mem_, AES_SIZE);
 
+    // mkeyを暗号化するための鍵生成
     key_gen_c crypt_key;
     if (!crypt_key.set_pass(pass_word)) {
         ERROR("パスワードをセットできません");
@@ -64,7 +97,36 @@ bool master_key_c::init(string usb_id, string pass_word)
         ERROR("鍵生成ができませんでした");
         return false;
     }
-    crypt_key.get_key();
+    if (!this->enc_mkey(crypt_key.get_key())) {
+        ERROR("mkeyの暗号化に失敗");
+        return false;
+    }
+    return true;
+}
+
+bool master_key_c::enc_mkey(const dynamic_mem_c key) const
+{
+    dynamic_mem_c enc_mkey;
+    if (!this->isCorrect()) {
+        ERROR("mkeyが格納されていません");
+        return false;
+    }
+    dynamic_mem_c iv(AES_SIZE);
+    RAND_bytes(iv.mem_, AES_SIZE);
+    aes_c aes;
+    aes.set_iv_key(iv, key);
+    enc_mkey.d_new(AES_SIZE);
+    aes.encrypt(enc_mkey, this->get_key(), aes_c::AES_bit_e::aes_256);
+    if (enc_mkey.size() != AES_SIZE) {
+        ERROR("鍵のサイズが不正");
+        log::push_value("enc_mkey.size()", enc_mkey.size());
+        return false;
+    }
+    if (!this->push_key(enc_mkey, iv, key)) {
+        ERROR("file output error");
+        return false;
+    }
+    return true;
 }
 
 vector<u_char> master_key_c::get_hash() const
@@ -76,9 +138,7 @@ vector<u_char> master_key_c::get_hash() const
         return result;
     }
     SHA_c sha;
-    dynamic_mem_c mkey;
-    mkey.from_vector(this->master_key_);
     dynamic_mem_c hash(AES_SIZE);
-    sha.sha2_cal(mkey, hash, SHA_c::SHA2_bit::SHA_256);
+    sha.sha2_cal(this->get_key(), hash, SHA_c::SHA2_bit::SHA_256);
     return result = hash.to_vector();
 }
